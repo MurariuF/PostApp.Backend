707cb6cae1f67c98ed19af15ccf3e053f4b3818e
MurariuF <murariu.flavius@gmail.com>
2023-02-09 15:32:54 +0200

    update

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,38 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PostApp.Backend.Models;
+using System.Data.SqlClient;
+
+namespace PostApp.Backend.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IConfiguration config;
+
+        public UsersController(IConfiguration config)
+        {
+            this.config = config;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Users>>> Users()
+        {
+            var sqlQuery = @"Select * 
+                            FROM Users";
+            using var connection = new SqlConnection(config.GetConnectionString("myDb1"));
+            var users = await connection.QueryAsync<Users>(sqlQuery);
+            return Ok(users);
+        }
+
+        [HttpGet("{userEmail}")]
+        public async Task<ActionResult<Users>> Users(string userEmail)
+        {
+            using var connection = new SqlConnection(config.GetConnectionString("myDb1"));
+            var user = await connection.QueryFirstAsync<Users>("Select * FROM Users WHERE Email = @Email", new { Email = userEmail });
+            return Ok(user);
+        }
+    }
+}
---
+++ Requests/PostRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace PostApp.Backend.Requests
+{
+    public class PostRequest
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public string Content { get; set; }
+    }
+}
